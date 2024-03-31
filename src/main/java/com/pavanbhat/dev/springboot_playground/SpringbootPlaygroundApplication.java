b97c0c2518c87c895fdf814d70f200cf2da2cd82
Pavan Bhat <363pavanbhat@gmail.com>
2024-03-31 17:06:10 +0530

    Initialized spring project

---
+++ src/main/java/com/pavanbhat/dev/springboot_playground/SpringbootPlaygroundApplication.java
@@ -0,0 +1,15 @@
+package com.pavanbhat.dev.springboot_playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPlaygroundApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringbootPlaygroundApplication.class, args
+		);
+	}
+
+}
