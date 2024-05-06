386543761c7b908d96c1ae6b86f45a161fa955aa
Pavan Bhat <363pavanbhat@gmail.com>
2024-05-06 23:59:10 +0530

    Adding Database connection

--- src/main/java/com/pavanbhat/dev/springboot_playground/SpringbootPlaygroundApplication.java
+++ src/main/java/com/pavanbhat/dev/springboot_playground/SpringbootPlaygroundApplication.java
@@ -1,7 +1,9 @@
 package com.pavanbhat.dev.springboot_playground;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication()
 //@SpringBootApplication(
@@ -16,6 +18,16 @@ public class SpringbootPlaygroundApplication {
 
 		SpringApplication.run(SpringbootPlaygroundApplication.class, args
 		);
+	}
+
+	//executed after spring beans have been loaded
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args){
+
+		//java lambda expression
+		return runner -> {
+			System.out.println("hello world from runner");
+		};
 	}
 
 }
