afdf98eb9861b9e8cf064b3b742d86b672496b72
Pavan Bhat <363pavanbhat@gmail.com>
2024-05-10 00:12:42 +0530

    query-1

---
+++ DBqueries/sql_queries.sql
@@ -0,0 +1,6 @@
+--1, start the id indexing from 3000
+
+ALTER TABLE student_tracker.student AUTO_INCREMENT=3000
+
+--2, to reset data and start from auto increment from 1, truncate data
+TRUNCATE student_tracker.student
