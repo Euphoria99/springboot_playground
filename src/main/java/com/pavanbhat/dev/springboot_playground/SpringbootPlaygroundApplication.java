c925300559293088890975012a050743cddc9711
pavan <363pavanbhat@gmail.com>
2024-05-16 22:18:16 +0530

    Update method

--- src/main/java/com/pavanbhat/dev/springboot_playground/SpringbootPlaygroundApplication.java
+++ src/main/java/com/pavanbhat/dev/springboot_playground/SpringbootPlaygroundApplication.java
@@ -10,45 +10,59 @@ import org.springframework.context.annotation.Bean;
 import java.util.List;
 
 //s3: update the main app
-@SpringBootApplication()
-//@SpringBootApplication(
-//		scanBasePackages = {
-//				"com.pavanbhat.dev.springboot_playground",
-//				"com.pavanbhat.dev.utils"
-//		}
-//)
+//@SpringBootApplication
+@SpringBootApplication(
+		scanBasePackages = {
+				"com.pavanbhat.dev.springboot_playground"
+		}
+)
 public class SpringbootPlaygroundApplication {
 
 	public static void main(String[] args) {
-
-		SpringApplication.run(SpringbootPlaygroundApplication.class, args
-		);
+		SpringApplication.run(SpringbootPlaygroundApplication.class, args);
 	}
 
-	//executed after spring beans have been loaded
 	@Bean
-	public CommandLineRunner commandLineRunner(StudentDAO studentDAO){
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 
-	return runner -> {
-//		createStudent(studentDAO);
+		return runner -> {
+			// createStudent(studentDAO);
 
-//		createMultipeStudent(studentDAO);
+			// createMultipleStudents(studentDAO);
 
-//		readStudent(studentDAO);
+			// readStudent(studentDAO);
 
-//		queryForStudents(studentDAO);
+			// queryForStudents(studentDAO);
 
-		queryForStudentsByLastName(studentDAO);
-	};
+			// queryForStudentsByLastName(studentDAO);
 
+			updateStudent(studentDAO);
 
+		};
+	}
 
+	private void updateStudent(StudentDAO studentDAO) {
+
+		// retrieve student based on the id: primary key
+		int studentId = 1;
+		System.out.println("Getting student with id: " + studentId);
+		Student myStudent = studentDAO.findById(studentId);
+
+		// change first name to "John"
+		System.out.println("Updating student ...");
+		myStudent.setFirstName("Vini");
+
+		// update the student
+		studentDAO.update(myStudent);
+
+		// display the updated student
+		System.out.println("Updated student: " + myStudent);
 	}
 
 	private void queryForStudentsByLastName(StudentDAO studentDAO) {
 
 		// get a list of students
-		List<Student> theStudents = studentDAO.findByLastName("Kang");
+		List<Student> theStudents = studentDAO.findByLastName("Doe");
 
 		// display list of students
 		for (Student tempStudent : theStudents) {
@@ -58,47 +72,47 @@ public class SpringbootPlaygroundApplication {
 
 	private void queryForStudents(StudentDAO studentDAO) {
 
-		//query a list of students
+		// get a list of students
 		List<Student> theStudents = studentDAO.findAll();
 
-		//display list of students
-		for(Student tempStudent : theStudents){
+		// display list of students
+		for (Student tempStudent : theStudents) {
 			System.out.println(tempStudent);
-	   }
-   }
+		}
+	}
+
 	private void readStudent(StudentDAO studentDAO) {
 
-		// create a student object
-		System.out.println("Creating a student object");
-		Student tempStudent = new Student("Daffy", "Duck", "daffy@fastnfurious.com");
+		// create  a student object
+		System.out.println("Creating new student object ...");
+		Student tempStudent = new Student("Daffy", "Duck", "daffy@luv2code.com");
 
-		//save the student
-		System.out.println("Saving the student...");
+		// save the student
+		System.out.println("Saving the student ...");
 		studentDAO.save(tempStudent);
 
-		//display id of the saved student
+		// display id of the saved student
 		int theId = tempStudent.getId();
 		System.out.println("Saved student. Generated id: " + theId);
 
-		//retrieve student based on the id: primary key
+		// retrieve student based on the id: primary key
 		System.out.println("Retrieving student with id: " + theId);
 		Student myStudent = studentDAO.findById(theId);
 
-		//display student
+		// display student
 		System.out.println("Found the student: " + myStudent);
 	}
 
-	private void createMultipeStudent(StudentDAO studentDAO){
+	private void createMultipleStudents(StudentDAO studentDAO) {
 
-		//create the student object
-		System.out.println("Creating new student object...");
-		Student tempStudent1 = new Student("Vin","Diesel", "vindiesel@fastnfurious.com");
-		Student tempStudent2 = new Student("Sung","Kang", "sungkang@fastnfurious.com");
-		Student tempStudent3 = new Student("Sung","Kang", "sungkang@fastnfurious.com");
+		// create multiple students
+		System.out.println("Creating 3 student objects ...");
+		Student tempStudent1 = new Student("John", "Doe", "john@luv2code.com");
+		Student tempStudent2 = new Student("Mary", "Public", "mary@luv2code.com");
+		Student tempStudent3 = new Student("Bonita", "Applebum", "bonita@luv2code.com");
 
-
-		//save the student object
-		System.out.println("Saving the student....");
+		// save the student objects
+		System.out.println("Saving the students ...");
 		studentDAO.save(tempStudent1);
 		studentDAO.save(tempStudent2);
 		studentDAO.save(tempStudent3);
@@ -106,17 +120,15 @@ public class SpringbootPlaygroundApplication {
 
 	private void createStudent(StudentDAO studentDAO) {
 
-		//create the student object
-		System.out.println("Creating new student object...");
-		Student tempStudent = new Student("Paul","Walker", "paulwalker@fastnfurious.com");
+		// create the student object
+		System.out.println("Creating new student object ...");
+		Student tempStudent = new Student("Paul", "Doe", "paul@luv2code.com");
 
-		//save the student object
-		System.out.println("Saving the student....");
+		// save the student object
+		System.out.println("Saving the student ...");
 		studentDAO.save(tempStudent);
 
-		//display the id of student object
-		System.out.println("Saved student. Generated id " + tempStudent.getId());
-
+		// display id of the saved student
+		System.out.println("Saved student. Generated id: " + tempStudent.getId());
 	}
-
 }
--- src/main/java/com/pavanbhat/dev/springboot_playground/dao/StudentDAOImpl.java
+++ src/main/java/com/pavanbhat/dev/springboot_playground/dao/StudentDAOImpl.java
@@ -13,18 +13,16 @@ import java.util.List;
 @Repository
 public class StudentDAOImpl implements StudentDAO {
 
-    //define field for entity manager
+    // define field for entity manager
     private EntityManager entityManager;
 
-
-    //inject entity manager using constructor injection
-
-
+    // inject entity manager using constructor injection
     @Autowired
     public StudentDAOImpl(EntityManager entityManager) {
         this.entityManager = entityManager;
     }
 
+    // implement save method
     @Override
     @Transactional
     public void save(Student theStudent) {
@@ -33,24 +31,15 @@ public class StudentDAOImpl implements StudentDAO {
 
     @Override
     public Student findById(Integer id) {
-        return entityManager.find(Student.class, 1);
+        return entityManager.find(Student.class, id);
     }
 
     @Override
     public List<Student> findAll() {
-       //create query
-
+        // create query
         TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student", Student.class);
 
-        //order by desc
-        //TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student order by lastName desc", Student.class);
-
-        // order by asc (asc is default for 'order by')
-        // TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student order by lastName desc", Student.class);
-
-        //return query results
-
-
+        // return query results
         return theQuery.getResultList();
     }
 
@@ -66,4 +55,11 @@ public class StudentDAOImpl implements StudentDAO {
         // return query results
         return theQuery.getResultList();
     }
+
+    @Override
+    @Transactional
+    public void update(Student theStudent) {
+        entityManager.merge(theStudent);
+    }
+
 }
