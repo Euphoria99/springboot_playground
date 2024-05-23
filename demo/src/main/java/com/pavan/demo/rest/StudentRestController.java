d6650e0bd13af5292ccfa98c53adc01ed4f56bf3
Pavan Bhat <363pavanbhat@gmail.com>
2024-05-23 22:47:59 +0530

    Using @PostConstruct to create students

--- demo/src/main/java/com/pavan/demo/rest/StudentRestController.java
+++ demo/src/main/java/com/pavan/demo/rest/StudentRestController.java
@@ -1,6 +1,7 @@
 package com.pavan.demo.rest;
 
 import com.pavan.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,15 +13,22 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    //define an endpoint
-    @GetMapping("/students")
-    public List<Student> getStudents(){
 
-        List<Student> theStudents = new ArrayList<>();
+    private List<Student> theStudents;
 
+    //define @PostConstruct to load the students data..only once
+
+    @PostConstruct
+    public void loadData(){
+        theStudents = new ArrayList<>();
         theStudents.add(new Student("Mike","Hussey"));
         theStudents.add(new Student("Mitcheal","Johnson"));
         theStudents.add(new Student("George","Bailey"));
+    }
+
+    //define an endpoint
+    @GetMapping("/students")
+    public List<Student> getStudents(){
         return theStudents;
     }
 }
