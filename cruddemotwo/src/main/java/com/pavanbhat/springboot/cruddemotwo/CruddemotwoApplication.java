8014ed45a5f06015f5c9a189014e46af910404a2
Pavan Bhat <363pavanbhat@gmail.com>
2024-05-26 03:43:07 +0530

    cruddemotwo init

---
+++ cruddemotwo/src/main/java/com/pavanbhat/springboot/cruddemotwo/CruddemotwoApplication.java
@@ -0,0 +1,13 @@
+package com.pavanbhat.springboot.cruddemotwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruddemotwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemotwoApplication.class, args);
+	}
+
+}
---
+++ cruddemotwo/src/test/java/com/pavanbhat/springboot/cruddemotwo/CruddemotwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavanbhat.springboot.cruddemotwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemotwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
