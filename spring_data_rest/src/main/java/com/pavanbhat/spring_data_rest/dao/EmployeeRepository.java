aeb0091af901fff038e8de116d3f74173f8f8ced
Pavan Bhat <363pavanbhat@gmail.com>
2024-06-02 00:02:12 +0530

    Changing page size,sorting with asc or desc

--- spring_data_rest/src/main/java/com/pavanbhat/spring_data_rest/dao/EmployeeRepository.java
+++ spring_data_rest/src/main/java/com/pavanbhat/spring_data_rest/dao/EmployeeRepository.java
@@ -4,7 +4,7 @@ import com.pavanbhat.spring_data_rest.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path="members")
+//@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     //
 }
