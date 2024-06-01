44d52d9cd324ec0ac81f4d853e206783da0ab96d
Pavan Bhat <363pavanbhat@gmail.com>
2024-06-01 23:44:56 +0530

    Changing the default REST endpoint

--- spring_data_rest/src/main/java/com/pavanbhat/spring_data_rest/dao/EmployeeRepository.java
+++ spring_data_rest/src/main/java/com/pavanbhat/spring_data_rest/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.pavanbhat.spring_data_rest.dao;
 
 import com.pavanbhat.spring_data_rest.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     //
 }
